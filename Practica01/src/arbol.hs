775d5a9da2d5c86f9b0ded425403e87dc7e15c88
Victor Gallegos <316160456@ciencias.unam.mx>
2021-03-23 20:33:20 -0600

    Estructura

--- Practica01/src/arbol.hs
+++ Practica01/src/arbol.hs
@@ -6,24 +6,24 @@ data BTree a = Void | Node a (BTree a) (BTree a) deriving (Show, Eq)
  - 1. Regresa el número de nodos de un árbol.
  - -}
 nNodes :: BTree a -> Int
-nNodes Void = 0
+nNodes Void           = 0
 nNodes (Node x ni nd) = 1 + nNodes ni + nNodes nd
 
 {-
  - 2. Regresa el número de hojas de un árbol.
  - -}
 nLeaves :: BTree a -> Int
-nLeaves Void = 0
+nLeaves Void               = 0
 nLeaves (Node x Void Void) = 1
-nLeaves (Node x ni nd) = nLeaves ni + nLeaves nd
+nLeaves (Node x ni nd)     = nLeaves ni + nLeaves nd
 
 {-
  - 3. Regresa el número de nodos internos de un árbol
  - -}
 nni :: BTree a -> Int
-nni Void = 0
+nni Void               = 0
 nni (Node x Void Void) = 0
-nni (Node x ni nd) = 1 + nni ni + nni nd
+nni (Node x ni nd)     = 1 + nni ni + nni nd
 
 {-
  - 4. Nos dice si un elemento está contenido en un árbol ordenado.
